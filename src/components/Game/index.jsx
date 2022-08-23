c886a14c940ab08c44ff4d7f21b9bee834a186cf
Miguel Quintero <quinteromdavid@gmail.com>
2022-08-22 22:54:29 -0430

    Added styling to Cards, Added Some Routes to App, Added NavLinks

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,9 +1,18 @@
-export default function Game() {
+import './styles.scss'
+export default function Game({ id, title, thumbnail, genre, platform }) {
+
     return (
         <div className="card">
-            <img src="https://www.freetogame.com/g/530/thumbnail.jpg" alt="this"/>
-            <h3>Title</h3>
-            <span>platform</span>
+            <div className="img-wrapper">
+                <img src={thumbnail} alt={title}/>
+            </div>
+            <div className="content">
+                <h3>{title}</h3>
+                <div className="content-category">
+                    <span className="platform">{platform}</span>
+                    <span className="genre">{genre}</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Games/index.jsx
+++ src/components/Games/index.jsx
@@ -1,14 +1,26 @@
 import Game from '../Game'
 import Loading from '../Loading'
 import useGames from '../../hooks/useGames'
+import './styles.scss'
 export default function Games() {
     const {games, setGames, loading} = useGames('/games')
+
     return (
-        <>
+        <div className="card-container">
             {loading ?
                 <Loading /> :
-                games.map(game => (<Game />)
-            )}
-        </>
+                games.map(game => {
+                    return (
+                        <Game
+                            key={game.id}
+                            id={game.id}
+                            title={game.title}
+                            thumbnail={game.thumbnail}
+                            genre={game.genre}
+                            platform={game.platform}
+                        />
+                    )
+                })}
+        </div>
     )
 }
